e20acdb26ebb0b9134bace8c050e355fd9b0eba0
Jake Dobler <jaecob.dobler@reusser.com>
2023-10-07 16:50:11 -0400

    changed UX flow, removed select number of players and names

--- src/application/GameBoard.java
+++ src/application/GameBoard.java
@@ -5,12 +5,14 @@ package application;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.collections.FXCollections;
-import javafx.event.EventHandler;
+
 import javafx.geometry.Pos;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+
+// Started by Jake Dobler
 
 public class GameBoard {
 	BorderPane board;
@@ -21,35 +23,43 @@ public class GameBoard {
 	}
 	
 	// Show a start screen
+	// Allow user to move to player select
 	private void displayStartScreen() {
-		EventHandler<MouseEvent> eventHandler = new EventHandler<MouseEvent>() {
-
-			@Override
-			public void handle(MouseEvent arg0) {
-				// TODO Auto-generated method stub
-				displayPlayerSelect();
-			}
-			
-		};
+		
 		Label title = new Label("Clue");
 		title.getStyleClass().add("title");
 		HBox titleBar = new HBox(title);
 		titleBar.setAlignment(Pos.CENTER);
 		Button start = new Button("Start");
-		start.addEventFilter(MouseEvent.MOUSE_CLICKED, eventHandler);
+		start.setOnAction(e -> {
+			
+		displayGameBoard();
+		});
 		board.setTop(titleBar);
 		board.setCenter(start);
 	}
 	
-	private void displayPlayerSelect() {
-		Label playersLabel = new Label("Number of Players:");
-		playersLabel.getStyleClass().add("title");
-		board.setTop(playersLabel);
-		String playerOptions[] = {"1", "2", "3", "4"};
-		ComboBox<String> cmb = new ComboBox(FXCollections.observableArrayList(playerOptions));
-		board.setCenter(cmb);
+	private void displayGameBoard() {
 		
 	}
+	
+//	private void displayPlayerSelect() {
+//		Label playersLabel = new Label("Number of Players:");
+//		playersLabel.getStyleClass().add("title");
+//		board.setTop(playersLabel);
+//		String playerOptions[] = {"1", "2", "3", "4"};
+//		ComboBox<String> cmb = new ComboBox(FXCollections.observableArrayList(playerOptions));
+//		board.setCenter(cmb);
+//		Button play = new Button("Play!");
+//		
+//		board.setBottom(play);
+//		
+//		play.setOnAction(e->{
+//			int numOfPlayers = Integer.parseInt(cmb.getValue());
+//			createPlayers(numOfPlayers);
+//		});
+//		
+//	}
 	
 	// Add Rooms to GUI (Should Rooms just be oversized tiles) I.E. Rooms extend tile?
 	
@@ -71,6 +81,29 @@ public class GameBoard {
 		board.getChildren().add(new Label(currentPlayer.name));
 		
 	}
+	
+	
+	// Stinky Code
+//	private void createPlayers(int num) {
+//		board.getChildren().clear();
+//		VBox inputs = new VBox();
+//		for(int i = 0; i < num ; i++) {
+//			// Show an input for each players
+//			int curPlayerNum = i +1;
+//			Label nameLabel = new Label("Player #" + curPlayerNum + " name:");
+//			TextField name = new TextField();
+//			HBox nameHB = new HBox(nameLabel, name);
+//			
+//			inputs.getChildren().add(nameHB);
+//		}
+//		Button submit = new Button("Submit");
+//		inputs.getChildren().add(submit);
+//		board.setCenter(inputs);
+//		submit.setOnAction(e->{
+//			Player p1 = Player()
+//		});
+//	}
+	
 	
 	
 	
