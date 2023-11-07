2f8f37e1b1ba70b71b8596c0515830f31ef236df
Jake Dobler <jaecob.d1@gmail.com>
2023-11-06 19:48:52 -0500

    made changes

--- src/application/Player.java
+++ src/application/Player.java
@@ -9,15 +9,16 @@ public class Player {
 	Player next;
 	Paint color;
 	Tile currentTile;
-	
+
 	public Player(String name, GraphicsContext gc, Paint color, Tile currentTile) {
-		
+
 		this.color = color;
 		this.currentTile = currentTile;
+		// comment
 		gc.setFill(color);
 		gc.fillOval((currentTile.x * 50) + 10, (currentTile.y * 50) + 10, 25, 25);
 	}
-	
+
 	public void movePlayer(Tile currentTile) {
 		this.currentTile = currentTile;
 	}
