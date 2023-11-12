eab7a49ae575de93876800be476589f418f900d1
Genevieve M <129635615+CatalystRose@users.noreply.github.com>
2023-11-12 16:35:31 -0500

    Update Room.java
    Adding comments to my stub

--- src/application/Room.java
+++ src/application/Room.java
@@ -5,7 +5,7 @@ public class Room extends Clue{
 	public Room(String name) {
 		super(name);
 	}
-
+// The following code section is already in the Game Board section. I am not sure we need to have a separate java file for the rooms, but if so it would be appropriate to have it here.
 	String rooms[] = {
 			"BallRoom",
 			"Billards Room",
