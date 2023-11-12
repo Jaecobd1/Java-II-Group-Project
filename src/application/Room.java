517f9df95f39f3d1bfca7834839e9b3808c12425
Genevieve M <129635615+CatalystRose@users.noreply.github.com>
2023-11-12 16:32:29 -0500

    Update Room.java

--- src/application/Room.java
+++ src/application/Room.java
@@ -5,4 +5,16 @@ public class Room extends Clue{
 	public Room(String name) {
 		super(name);
 	}
+
+	String rooms[] = {
+			"BallRoom",
+			"Billards Room",
+			"Conservatory",
+			"Dining Room",
+			"Hall",
+			"Kitchen",
+			"Lounge",
+			"Libaray",
+			"Study",
+	};
 }
