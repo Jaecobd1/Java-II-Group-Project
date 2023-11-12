8bcf83d241047c52d482ee8b58f62c8b6437d62b
Genevieve M <129635615+CatalystRose@users.noreply.github.com>
2023-11-12 16:31:54 -0500

    Update Weapon.java

--- src/application/Weapon.java
+++ src/application/Weapon.java
@@ -4,4 +4,13 @@ public class Weapon extends Clue{
 	public Weapon(String name) {
 		super(name);
 	}
+
+	String weapons[] = {
+			"Knife",
+			"Revolver",
+			"Rope",
+			"Wrench",
+			"Candelstick",
+			"Lead Pipe",
+	};
 }
