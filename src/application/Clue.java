30c8e78d7d6248a7d651b0c1fbcabdf1af4aa3e4
Genevieve M <129635615+CatalystRose@users.noreply.github.com>
2023-11-12 18:26:36 -0500

    Update Clue.java
    Committed manual change because I am having trouble pushing from my clone on Netbeans.

--- src/application/Clue.java
+++ src/application/Clue.java
@@ -1,5 +1,6 @@
 package application;
 // Started by Jake Dobler
+// Pseudocode stubs by Genevieve Mathews
 abstract public class Clue {
 	String clueName;
 
@@ -8,4 +9,41 @@ abstract public class Clue {
 		this.clueName = name;
 	}
 	
+        /**
+        * Accusation Pseudocode:
+        *Print "Make Accusations?"
+        *Player: Input Y/N
+        * If Y:
+        * Print "Enter Suspect: "
+        * Player: Inputs Suspect #
+        * Print "Enter Item: "
+        * Player Inputs Item #
+        * Print "Enter Location: "
+        * Player Inputs Location #
+        * 
+        * Checker program here
+        * If Suspect = true
+        * print true
+        * else false
+
+        * If Item = true
+        * print true
+        * else false
+        * If Location = true
+        * print true
+        * else false
+         * /
+        
+        /**
+         * Accusation Checker.
+         * Pseudocode idea:
+         * Solution = (Suspect=Professor Plum, Item= Candlestick, Room = Study)
+         * Player Guess = (Suspect=Colonel Mustard, Item= Candlestick, Room=Billiard Room)
+         * If/then checker here
+         * return:
+         * Suspect = false
+         * Item = true
+         * Room = false
+         */
+
 }
