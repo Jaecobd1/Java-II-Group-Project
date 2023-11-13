9f401ed56efe75fdc577a659c2655ec6de032f16
DanielvReyes <147454328+DanielvReyes@users.noreply.github.com>
2023-11-13 16:37:51 -0600

    Add files via upload

---
+++ src/application/Assumptions.java
@@ -0,0 +1,62 @@
+// Project Name: Clue
+// Started by: Daniel Reyes
+// Description: Assumptions page, when user reaches a room creates pop up with drop down menu
+// for categories.
+
+package application;
+
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+
+public class Assumptions {
+
+    // Pop up window.
+    private Stage stage;
+    
+    // The drop down menus.
+    private ComboBox<String> dropSuspect;
+    private ComboBox<String> dropWeapon;
+    private ComboBox<String> dropRoom;
+    
+    // Initializes the pop up and sets up its components.
+    public Assumptions() 
+    {
+    // Initialize stage and layout
+    // Initialize components
+    // Use method to configure components
+    // Set up layout
+    // Place button
+    // Set scene   
+    }
+    
+    // Configures the drop downs, button, etc.
+    private void configComponents() 
+    {
+    // Configure drop downs
+    // Set up event handlers if needed
+    }
+    
+    // Used to display the pop up on the GameBoard.
+    public void show() 
+    {
+    // Show pop up
+    }
+    
+    // Retrieve selected values from drop downs
+    // Should return to use the data as needed
+    public void getData() 
+    {
+    // get values
+    // return values
+    }
+    
+    // Creates a random assumption for NPC
+    public void ranAssumption() 
+    {
+        // Generate value for each category
+        // return value
+    }
+
+}
