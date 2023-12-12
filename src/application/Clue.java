00e0bf680e98c3dd9e240ff5e1e6d6aeb679d41d
Genevieve M <129635615+CatalystRose@users.noreply.github.com>
2023-12-11 19:03:44 -0500

    Update Clue.java

--- src/application/Clue.java
+++ src/application/Clue.java
@@ -1,49 +1,38 @@
 package application;
 // Started by Jake Dobler
-// Pseudocode stubs by Genevieve Mathews
+// Code by Genevieve Mathews
+
 abstract public class Clue {
-	String clueName;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-	public Clue(String name) {
-		// TODO Auto-generated constructor stub
-		this.clueName = name;
-	}
-	
-        /**
-        * Accusation Pseudocode:
-        *Print "Make Accusations?"
-        *Player: Input Y/N
-        * If Y:
-        * Print "Enter Suspect: "
-        * Player: Inputs Suspect #
-        * Print "Enter Item: "
-        * Player Inputs Item #
-        * Print "Enter Location: "
-        * Player Inputs Location #
-        * 
-        * Checker program here
-        * If Suspect = true
-        * print true
-        * else false
+        // Accusation Input
+        System.out.println("Make Accusations?");
+        System.out.print("Player: Input Y/N: ");
+        char input = scanner.next().charAt(0);
 
-        * If Item = true
-        * print true
-        * else false
-        * If Location = true
-        * print true
-        * else false
-         * /
-        
-        /**
-         * Accusation Checker.
-         * Pseudocode idea:
-         * Solution = (Suspect=Professor Plum, Item= Candlestick, Room = Study)
-         * Player Guess = (Suspect=Colonel Mustard, Item= Candlestick, Room=Billiard Room)
-         * If/then checker here
-         * return:
-         * Suspect = false
-         * Item = true
-         * Room = false
-         */
+        if (input == 'Y' || input == 'y') {
+            System.out.print("Enter Suspect: ");
+            int suspect = scanner.nextInt();
 
-}
+            System.out.print("Enter Item: ");
+            int item = scanner.nextInt();
+
+            System.out.print("Enter Location: ");
+            int location = scanner.nextInt();
+
+            // Checker program here
+            boolean suspectResult = checkSuspect(suspect);
+            boolean itemResult = checkItem(item);
+            boolean locationResult = checkLocation(location);
+
+            // Output results
+            System.out.println("Suspect: " + suspectResult);
+            System.out.println("Item: " + itemResult);
+            System.out.println("Location: " + locationResult);
+        } else {
+            System.out.println("No accusations made.");
+        }
+
+        scanner.close();
+    }
