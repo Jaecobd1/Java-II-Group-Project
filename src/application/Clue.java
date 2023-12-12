85ed04327d3964197fd6e126ae55b637ce1db1a1
Genevieve M <129635615+CatalystRose@users.noreply.github.com>
2023-12-11 19:04:25 -0500

    Update Clue.java

--- src/application/Clue.java
+++ src/application/Clue.java
@@ -36,3 +36,59 @@ abstract public class Clue {
 
         scanner.close();
     }
+
+// Accusation Checker
+    public static void accusationChecker(int playerSuspect, int playerItem, int playerLocation) {
+        // Solution
+        String solutionSuspect = "Professor Plum";
+        String solutionItem = "Candlestick";
+        String solutionLocation = "Study";
+
+        // Player Guess
+        String playerGuessSuspect = getSuspectName(playerSuspect);
+        String playerGuessItem = getItemName(playerItem);
+        String playerGuessLocation = getLocationName(playerLocation);
+
+        // If/then checker
+        boolean suspectResult = playerGuessSuspect.equals(solutionSuspect);
+        boolean itemResult = playerGuessItem.equals(solutionItem);
+        boolean locationResult = playerGuessLocation.equals(solutionLocation);
+
+        // Return results
+        System.out.println("Suspect: " + suspectResult);
+        System.out.println("Item: " + itemResult);
+        System.out.println("Location: " + locationResult);
+    }
+
+    // Helper methods to get names based on input numbers
+    public static String getSuspectName(int suspect) {
+        // Implement logic to get suspect name based on the input number
+        return "";
+    }
+
+    public static String getItemName(int item) {
+        // Implement logic to get item name based on the input number
+        return "";
+    }
+
+    public static String getLocationName(int location) {
+        // Implement logic to get location name based on the input number
+        return "";
+    }
+
+    // Placeholder methods for checking
+    public static boolean checkSuspect(int suspect) {
+        // Implement logic to check the suspect
+        return false;
+    }
+
+    public static boolean checkItem(int item) {
+        // Implement logic to check the item
+        return false;
+    }
+
+    public static boolean checkLocation(int location) {
+        // Implement logic to check the location
+        return false;
+    }
+}
